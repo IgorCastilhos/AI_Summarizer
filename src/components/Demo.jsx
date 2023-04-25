4c3935f3182477b2f470611a7cee30554eac62aa
Igor Castilhos <igorcastilhos2010@hotmail.com>
2023-04-24 23:52:45 -0300

    Creating demo, hero and styling

---
+++ src/components/Demo.jsx
@@ -0,0 +1,48 @@
+import { useState, useEffect } from "react";
+import { copy, linkIcon, loader, tick } from "../assets";
+
+const Demo = () => {
+  // const [article, setArticle] = useState({
+  // url:
+  // })
+
+  return (
+    <section className="mt-16 w-full max-w-xl">
+      {/* Busca */}
+      <div className="flex flex-col gap-2 w-full">
+        <form
+          className="relative flex justify-center items-center"
+          onSubmit={() => {}}
+        >
+          <img
+            src={linkIcon}
+            alt="link_icon"
+            className="absolute left-0 my-2 ml-3 w-5"
+          />
+
+          <input
+            type="url"
+            placeholder="Digite uma URL"
+            value=""
+            onChange={() => {}}
+            required
+            className="url_input peer"
+          />
+
+          <button
+            type="submit"
+            className="submit_btn peer-focus:border-gray-700 peer-focus:text-gray-700"
+          >
+            ↵
+          </button>
+        </form>
+
+        {/* Histórico de URLs pesquisadas */}
+      </div>
+
+      {/* Exibe os resultados */}
+    </section>
+  );
+};
+
+export default Demo;
---
+++ src/components/Hero.jsx
@@ -0,0 +1,32 @@
+import { logo } from "../assets";
+
+const Hero = () => {
+  return (
+    <header className="w-full flex justify-center items-center flex-col">
+      <nav className="flex justify-between items-center w-full mb-10 pt-3">
+        <img src={logo} alt="sumarrize_logo" className="w-28 object-contain" />
+
+        <button
+          type="button"
+          onClick={() =>
+            window.open("https://github.com/IgorCastilhos", "_blank")
+          }
+          className="black_btn"
+        >
+          GitHub
+        </button>
+      </nav>
+
+      <h1 className="head_text">
+        Resuma Artigos com <br className="max-md:hidden" />
+        <span className="orange_gradient">OpenAI GPT-4</span>
+      </h1>
+      <h2 className="desc">
+        Simplifique sua leitura com o Summarizer, um projeto open-source que
+        transforma artigos longos em resumos claros e concisos
+      </h2>
+    </header>
+  );
+};
+
+export default Hero;
